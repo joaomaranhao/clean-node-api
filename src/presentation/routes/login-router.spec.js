3bb5514c758eead897bfb68eac16621b3cda748b
João Maranhão <joaofelipemaranhao@gmail.com>
2021-05-21 19:37:16 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -2,9 +2,13 @@ const { LoginRouter } = require('./login-router')
 const { MissingParamError } = require('../helpers/missing-param-error')
 const { InternalServerError } = require('../helpers/internal-server-error')
 
+const makeSut = () => {
+  return new LoginRouter()
+}
+
 describe('Login Router', () => {
-  const sut = new LoginRouter()
   test('should return 400 if no email is provided', () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -16,6 +20,7 @@ describe('Login Router', () => {
   })
 
   test('should return 400 if no password is provided', () => {
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@mail.com'
@@ -27,12 +32,14 @@ describe('Login Router', () => {
   })
 
   test('should return 500 if no httpRequest is provided', () => {
+    const sut = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new InternalServerError('http request'))
   })
 
   test('should return 500 if httpRequest has no body', () => {
+    const sut = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
