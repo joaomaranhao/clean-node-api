2ff2e37ea3cfa23335dca5c1ec86ea43318bee35
Joao Maranhao <joaofelipemaranhao@gmail.com>
2021-05-21 12:35:04 -0300

    test: ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routes/login-router.spec.js
+++ src/presentation/routes/login-router.spec.js
@@ -11,4 +11,14 @@ describe('Login Router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
   })
+
+  test('should return 400 if no password is provided', () => {
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
 })
