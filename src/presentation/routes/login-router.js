f2929dc1927645510fc40735354bb8959f2638e6
João Maranhão <joaofelipemaranhao@gmail.com>
2021-05-21 18:54:11 -0300

    feat: ensure LoginRouter returns 500 if an invalid httpRequest is provided

--- src/presentation/routes/login-router.js
+++ src/presentation/routes/login-router.js
@@ -1,7 +1,11 @@
 class LoginRouter {
   route (httpRequest) {
-    const { email, password } = httpRequest.body
     const httpResponse = {}
+    if (!httpRequest || !httpRequest.body) {
+      httpResponse.statusCode = 500
+      return httpResponse
+    }
+    const { email, password } = httpRequest.body
     if (!email || !password) {
       httpResponse.statusCode = 400
     }
