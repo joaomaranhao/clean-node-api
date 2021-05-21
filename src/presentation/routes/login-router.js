456ee7a6820062cf42005c5b3a88c4d6157818de
Joao Maranhao <joaofelipemaranhao@gmail.com>
2021-05-21 12:35:36 -0300

    feat: ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routes/login-router.js
+++ src/presentation/routes/login-router.js
@@ -1,7 +1,7 @@
 class LoginRouter {
   route (httpRequest) {
     const httpResponse = {}
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       httpResponse.statusCode = 400
     }
     return httpResponse
